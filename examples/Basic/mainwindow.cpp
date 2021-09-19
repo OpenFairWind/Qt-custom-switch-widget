c8a1178c06549e5f0dcae563094c9ec1060e71cd
Raffaele Montella <raffaele.montella@uniparthenope.it>
2021-09-19 22:51:00 +0200

    Update demo

--- examples/Basic/mainwindow.cpp
+++ examples/Basic/mainwindow.cpp
@@ -13,11 +13,43 @@ MainWindow::MainWindow(QWidget *parent) :
         QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
 
-    // Create the gauge widget
-    auto switchWidget = new QcSwitchWidget;
-    switchWidget->setState(false);
 
-    ui->horizontalLayout->addWidget(switchWidget);
+    // Create the switch widget
+
+    auto cabinLights1SwitchWidget = new QcSwitchWidget;
+    cabinLights1SwitchWidget->setDrawBezel(false);
+    cabinLights1SwitchWidget->setTextPosition(0);
+    cabinLights1SwitchWidget->setState(false);
+    cabinLights1SwitchWidget->setLabel("Cabin Lights 1");
+    ui->horizontalLayout->addWidget(cabinLights1SwitchWidget);
+
+    auto cabinLights2SwitchWidget = new QcSwitchWidget;
+    cabinLights2SwitchWidget->setDrawBezel(false);
+    cabinLights2SwitchWidget->setTextPosition(1);
+    cabinLights2SwitchWidget->setState(false);
+    cabinLights2SwitchWidget->setLabel("Cabin Lights 2");
+    ui->horizontalLayout->addWidget(cabinLights2SwitchWidget);
+
+    auto cabinLights3SwitchWidget = new QcSwitchWidget;
+    cabinLights3SwitchWidget->setDrawBezel(false);
+    cabinLights3SwitchWidget->setTextPosition(2);
+    cabinLights3SwitchWidget->setState(false);
+    cabinLights3SwitchWidget->setLabel("Cabin Lights 3");
+    ui->horizontalLayout->addWidget(cabinLights3SwitchWidget);
+
+    auto keelBilgePumpSwitchWidget = new QcSwitchWidget;
+    keelBilgePumpSwitchWidget->setTextPosition(3);
+    keelBilgePumpSwitchWidget->setState(false);
+    keelBilgePumpSwitchWidget->setBazelRadius(10);
+    keelBilgePumpSwitchWidget->setLabel("Keel Bilge Pump");
+    ui->horizontalLayout->addWidget(keelBilgePumpSwitchWidget);
+
+    auto anchorWindlassSwitchWidget = new QcSwitchWidget;
+    anchorWindlassSwitchWidget->setTextPosition(4);
+    anchorWindlassSwitchWidget->setState(false);
+    anchorWindlassSwitchWidget->setBazelRadius(10);
+    anchorWindlassSwitchWidget->setLabel("Anchor Windlass");
+    ui->horizontalLayout->addWidget(anchorWindlassSwitchWidget);
 }
 
 MainWindow::~MainWindow() {
